9a16344a602c05f6e83032923b795db709fb30a3
Taeil Lee <lee.taeil@gmail.com>
2022-07-28 15:42:16 +0900

    For문+If문 - CMYK Blend

---
+++ 2-4 for-if_cmyk/sketch.js
@@ -0,0 +1,40 @@
+
+const d = 20;
+const disp = 8;
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  blendMode(BLEND);
+  background(255);
+  blendMode(MULTIPLY);
+  noStroke();
+
+  let numh = width/(2*d);
+  let numv = height/(2*d);
+
+  for(let n=0; n<3; n++) {
+    for(let j=0; j<numv; j++) {
+      for(let i=0; i<numh; i++) {
+        let dx, dy;
+        if(n==0) {
+          fill(255, 255, 0);  //'#ffff00'
+          dx = 0;
+          dy = 0;
+        } else if (n==1) {
+          fill(255, 0, 255);  //'#ff00ff'
+          dx = map(i, 0, numh, -disp, disp); //i*0.4;
+          dy = 0;
+        } else {
+          fill(0, 255, 255);  //'#00fffd'
+          dx = 0;
+          dy = map(j, 0, numv, -disp, disp); //j*0.4;
+        }
+        ellipse(i*2*d+d + dx, j*2*d+d + dy, d);
+      }
+    }
+  }
+
+}
