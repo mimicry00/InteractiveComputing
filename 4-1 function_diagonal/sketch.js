407470d9bc3f4ba03be5e1c4be1e58dbe74eddb3
Taeil Lee <lee.taeil@gmail.com>
2022-07-31 14:23:34 +0900

    Update sketch.js

--- 4-1 function_diagonal/sketch.js
+++ 4-1 function_diagonal/sketch.js
@@ -7,10 +7,10 @@ function setup() {
 	strokeWeight(4);
 
 	for (let x = 0; x < width; x += step) {
-	 	for (let y = 0; y < height; y += step) {
-	 		drawDiagonal(x, y, step, step);
-	 	}
-  }
+		for (let y = 0; y < height; y += step) {
+			drawDiagonal(x, y, step, step);
+		}
+	}
 }
 
 function draw() {
