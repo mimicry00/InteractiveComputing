9be09264fbe49771924af582b120fc79467ac818
Taeil Lee <lee.taeil@gmail.com>
2022-07-31 16:38:23 +0900

    클래스(class) - arrows

---
+++ 5-1 class_arrows/sketch.js
@@ -0,0 +1,56 @@
+let triangles = [];
+
+function setup() {
+  createCanvas(800, 400);
+  colorMode(HSB);
+
+  for(let i=0; i<width; i+=20) {
+    for(let j=0; j<height; j+=20) {
+      triangles.push(new Triangle(i+10, j+10));
+    }
+  }
+}
+
+function draw() {
+  background(0);
+  noStroke();
+
+  for(let tri of triangles) {
+    tri.update();
+    tri.show();
+  }
+
+}
+
+class Triangle {
+  constructor(_x, _y) {
+    this.x = _x;
+    this.y = _y;
+    this.w = 20;
+    this.hue = 0;
+    this.rad = 0;
+  }
+
+  show() {
+    fill(this.hue, 100, 100);
+    
+    push();
+    translate(this.x, this.y);
+    rotate(this.rad);
+    beginShape();
+    vertex(this.w/2, 0);
+    vertex(-this.w/2, this.w/4);
+    vertex(-this.w/2, -this.w/4);
+    endShape();
+    pop();   
+  }
+  
+  update() {
+    let distance = dist(this.x, this.y, mouseX, mouseY);
+    let maxdistance = dist(0, 0, width, height);
+    this.w = map(distance, 0, maxdistance, 20, 0);    
+    this.hue = map(distance, 0, maxdistance, 0, 360);
+
+    this.rad = atan2(mouseY-this.y, mouseX-this.x);
+  }
+}
