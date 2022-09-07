ca34813aa210a22b000bc6c578b2bac80c702ccb
Taeil Lee <lee.taeil@gmail.com>
2022-09-07 18:52:31 +0900

    Update sketch.js

--- 4-1 function_diagonal/sketch.js
+++ 4-1 function_diagonal/sketch.js
@@ -2,24 +2,22 @@ let step = 40;
 
 function setup() {
 	createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+	noLoop();
+
 	background(255);
 	stroke(0);
 	strokeWeight(4);
 
 	for (let x = 0; x < width; x += step) {
 		for (let y = 0; y < height; y += step) {
-			drawDiagonal(x, y, step, step);
+			if (random(1) >= 0.5) {
+				line(x, y, x + step, y + step);
+			} else {
+				line(x + step, y, x, y + step);
+			}
 		}
-	}
-}
-
-function draw() {
-}
-
-function drawDiagonal(x, y, w, h) {
-	if (random(1) >= 0.5) {
-		line(x, y, x + w, y + h);
-	} else {
-		line(x + w, y, x, y + h);
 	}
 }
